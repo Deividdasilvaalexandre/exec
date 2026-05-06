cd89241a7c8f6e8776444e3743520e9b963bc23b
Deivid da Silva Alexandre <akirrabr0@gmail.com>
2026-05-06 14:51:56 -0300

    exec

---
+++ valores.js
@@ -0,0 +1,48 @@
+let categoria = 0
+
+calcularValorLuxo(luxo, precoFabricaLuxo, luxoValor);{
+ function luxoValor = precoFabricaLuxo * luxo
+}
+
+calcularValorSedan(sedan, precoFabricaSedan, sedanValor);{
+ function sedanValor = precoFabricaSedan * sedan
+}
+
+calcularValorPopular(popular, precoFabricaPopular, popularValor);{
+function popularValor = precoFabricaPopular * popular
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+principal(
+    luxo = 0.25
+    ,sedan = 0.20
+    ,popular = 0.15
+
+    ,precoFabricaLuxo = 220000
+    ,precoFabricaSedan =  105000
+    ,precoFabricaPopular = 60000
+
+    ,luxoValor
+    ,sedanValor
+    ,popularValor
+)
